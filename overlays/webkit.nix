aab628c98f209cb5769d2796fff7195e8b3309b8
Ron Parker <rdparker@gmail.com>
2023-05-09 11:57:09 -0400

    Only add webkit if the UI supports it

--- overlays/webkit.nix
+++ overlays/webkit.nix
@@ -6,20 +6,20 @@ let
 in {
   emacs = (super.emacs.override { }).overrideAttrs (oa:
     let
-      gui = any (x: builtins.elem x oa.configureFlags)
-        ([ "--with-ns" "--with-mac" "--with-pgtk" ]
-          ++ (forEach [ "gtk2" "gtk3" "motif" "athena" "lucid" ]
-            (x: "--with-x-toolkit=${x}")));
-    in {
-      buildInputs = oa.buildInputs ++ (if gui then
-        [
-          (if isDarwin then
-            super.darwin.apple_sdk.frameworks.WebKit
-          else
-            super.webkitgtk)
-        ]
+      supportedGui = any (x: builtins.elem x oa.configureFlags) [
+        "--with-ns"
+        "--with-mac"
+        "--with-x-toolkit=gtk3"
+        "--with-pgtk"
+      ];
+      webKit = if isDarwin then
+        super.darwin.apple_sdk.frameworks.WebKit
       else
-        [ ]);
-      configureFlags = oa.configureFlags ++ lib.optional gui "--with-xwidgets";
-    });
+        super.webkitgtk;
+
+    in if supportedGui then {
+      buildInputs = oa.buildInputs ++ [ webKit ];
+      configureFlags = oa.configureFlags ++ [ "--with-xwidgets" ];
+    } else
+      { });
 }
